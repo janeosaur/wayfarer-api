0a07a02bfd73e820a17c8e857ba08492ee6f0d70
janeosaur <janewie@gmail.com>
2017-05-12 11:57:18 -0700

    heroku deploy

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Wayfarer-api");
 
 
-mongoose.Promise = globa.Promise;
+mongoose.Promise = global.Promise;
 
 // exporting a key-value object, with key 'Comments'
 module.exports.Comment = require('./comments');
