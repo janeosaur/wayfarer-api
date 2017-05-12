cee71bacf87a9ccbd9df8e228885a26f86aff629
janeosaur <janewie@gmail.com>
2017-05-12 14:52:35 -0700

    heroku deployed, fix routes

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ var port = process.env.PORT || 3000;
 var dbUser = process.env.MLAB_DBUSER
 var dbPassword = process.env.MLAB_DBPASSWORD
 var databaseUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@ds133331.mlab.com:33331/mywayfarer'
-// mongoose.connect(databaseUrl)
-
+mongoose.connect(databaseUrl)
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -40,130 +39,108 @@ app.use(function(req, res, next) {
 app.use('/api', router);
 
 //set route path and init API
-router.get('/', function(req,res) {
+app.get('/api', function(req,res) {
   res.json({message: 'API Initialized for Wayfarer !!!'});
 });
 
-// delete all comments
-router.route('/comments/nuke').get(function(req,res){
-  db.Comment.remove(function(err,succ){
-  res.json(succ);
+// // delete all comments
+// app.get('/api/comments/nuke', function(req,res){
+//   db.Comment.remove(function(err,succ){
+//   res.json(succ);
+//   });
+// });
+
+// get all comments
+app.get('/api/comments', function(req,res) {
+  db.Comment.find(function(err, comments) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(comments);
   });
-});
+})
 
-
-//adding the /comments route to our /api router
-router.route('/comments')
-  .get(function(req, res) {
-    db.Comment.find(function(err, comments) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(comments);
-    });
+// create a comment
+app.post('/api/comments', function(req,res) {
+  var post = new db.Comment();
+  post.name = req.body.name;
+  post.title = req.body.title;
+  post.text = req.body.text;
+  post.city = req.body.city;
+  post.date = Date.now();
+  post.save();
+  db.City.findOne({name: post.city}, function(err, foundCity) {
+    if (err) {
+      console.log('post error at find one city');
+    } else {
+      console.log(foundCity.comments)
+      console.log(post._id)
+      foundCity.comments.push(post._id);
+      foundCity.save();
+      res.json(foundCity);
+    }
   })
-  .post(function(req,res) {
-    var post = new db.Comment();
-    post.name = req.body.name;
-    post.title = req.body.title;
-    post.text = req.body.text;
-    post.city = req.body.city;
-    post.date = Date.now();
-    post.save();
-      // function(err) {
-      // if (err) {
-      //   res.send(err);
-      // } res.json({ message: 'travel tip successfully added!' })
-      // "can't set headers after they're sent"
-      // console.log(post._id)
-      // res.json({ message: 'travel tip successfully added!' });
-    // });
-    db.City.findOne({name: post.city}, function(err, foundCity) {
+})
+
+// get one comment by id
+app.get('/api/comments/:id', function(req,res) {
+  db.Comment.findById(req.params.id, function(err, comments) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(comments);
+  });
+})
+
+// delete one comment by id
+app.delete('/api/comments/:id', function(req,res) {
+  db.Comment.remove({_id:req.params.id}, function(err, comment) {
+    if (err) {
+      res.send(err);
+    } res.json({message: 'comment has been deleted'})
+  })
+})
+
+// update one comment by id
+app.put('/api/comments/:id', function(req, res) {
+  db.Comment.findById(req.params.id, function(err, comment) {
+    if (err) {
+      res.send(err);
+    }
+    (req.body.name) ? comment.name = req.body.name : null;
+    (req.body.title) ? comment.title = req.body.title : null;
+    (req.body.text) ? comment.text = req.body.text : null;
+    (req.body.date) ? comment.date = req.body.date : null;
+    comment.save(function(err) {
       if (err) {
-        console.log('post error at find one city');
-      } else {
-        console.log(foundCity.comments)
-        console.log(post._id)
-        foundCity.comments.push(post._id);
-        foundCity.save();
-        res.json(foundCity);
+        res.send(err)
       }
+      res.json({message: 'comment has been updated'})
     })
   })
+})
 
-  // below worked for embedded comments
-  // .post(function(req, res) {
-  //   var post = new db.Comment();
-  //   post.name = req.body.name;
-  //   post.title = req.body.title;
-  //   post.text = req.body.text;
-  //   post.city = req.body.city;
-  //   post.date = Date.now();     //   need to store as time/date so can calculate how old a post is
-  //   post.save(function(err) {
-  //     if (err) {
-  //       res.send(err);
-  //     }
-  //     res.json({ message: 'travel tip successfully added!' });
-  //   });
-  // });
-
-router.route('/comments/:id')
-  .get(function(req, res) {
-    db.Comment.findById(req.params.id, function(err, comments) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(comments);
-    });
-  })
-  .delete(function(req,res) {
-    db.Comment.remove({_id:req.params.id}, function(err, comment) {
-      if (err) {
-        res.send(err);
-      } res.json({message: 'comment has been deleted'})
-    })
-  })
-  .put(function(req,res) {
-    db.Comment.findById(req.params.id, function(err, comment) {
-      if (err) {
-        res.send(err);
-      }
-      (req.body.name) ? comment.name = req.body.name : null;
-      (req.body.title) ? comment.title = req.body.title : null;
-      (req.body.text) ? comment.text = req.body.text : null;
-      (req.body.date) ? comment.date = req.body.date : null;
-      comment.save(function(err) {
-        if (err) {
-          res.send(err)
-        }
-        res.json({message: 'comment has been updated'})
-      })
-    })
-  })
-
-// get comments from one user name
-router.route('/profile/comments/:name')
-  .get(function(req, res) {
-    db.Comment.find({name: req.params.name}, function(err, comments) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(comments);
-    });
-  })
+// get comments from one username
+app.get('/api/profile/comments/:name', function(req, res) {
+  db.Comment.find({name: req.params.name}, function(err, comments) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(comments);
+  });
+})
 
 // get all cities
-router.route('/cities')
-  .get(function(req, res) {
-    db.City.find({})
-      .populate('comments')
-      .exec(function(err, cities) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(cities);
-      });
-    })
+app.get('/api/cities', function(req, res) {
+  db.City.find({})
+    .populate('comments')
+    .exec(function(err, cities) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(cities);
+    });
+  })
 
 // // get specific city info
 // router.route('/cities/:id')
@@ -176,18 +153,17 @@ router.route('/cities')
 //     });
 //   })
 
-// get specific city info
-router.route('/cities/:name')
-  .get(function(req, res) {
-    db.City.findOne({name: req.params.name})
-      .populate('comments')
-      .exec(function(err, city) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(city);
-      });
-    })
+// get one city by name
+app.get('/api/cities/:name', function(req, res) {
+  db.City.findOne({name: req.params.name})
+  .populate('comments')
+  .exec(function(err, city) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(city);
+  });
+})
 
 
 //start server
