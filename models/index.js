435b35dc807f40e6240bce3dd40c78f7cab64c49
janeosaur <janewie@gmail.com>
2017-05-12 11:54:41 -0700

    updated mongoose.connect

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Wayfarer");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Wayfarer-api");
+
+
+mongoose.Promise = globa.Promise;
 
 // exporting a key-value object, with key 'Comments'
 module.exports.Comment = require('./comments');
